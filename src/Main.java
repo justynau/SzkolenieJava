3d811ad8bbe8f80e03509fc448141436ffb07722
justynau <justyna.utko@ttpsc.pl>
2019-04-10 16:00:33 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
 public class Main {
 
     public static void main(String[] args) {
+        zad1();
+
 
 //        System.out.println("Hello World!");
-
-        boolean szkolenie = true;
-        int liczba = 10;
-        char a;
-        a = 'a';
-        String SzkolenieJava = "Szkolenie " + liczba +a;
-
+//
+//        boolean szkolenie = true;
+//        int liczba = 10;
+//        char a;
+//        a = 'a';
+//        String SzkolenieJava = "Szkolenie " + liczba +a;
+//
 //      sout drukowanie
-        // ctr + alt + l - formatowanie kodu
+//        // ctr + alt + l - formatowanie kodu
 //        shift f10 - start
+//
+//
+//        System.out.println(a);
+//        System.out.println(szkolenie);
+//        System.out.println(SzkolenieJava);
+//
+//    float x = (float) 10/3;
+//    double f = (double) 10/3;
+//    System.out.println("Float: " + x + " double: " + f);
+//
+    }
 
-
-        System.out.println(a);
-        System.out.println(szkolenie);
-        System.out.println(SzkolenieJava);
-
-
-
+    public static void zad1() {
+        System.out.println("hello");
     }
 
 }
+
+
