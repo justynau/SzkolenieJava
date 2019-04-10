3d3e9b88968a4228f93aa0504bbd63cfabfe186f
Justyna Utko <utkoj@tt.com.pl>
2019-04-10 15:27:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!");
+
+        boolean szkolenie = true;
+        int liczba = 10;
+        //char a = a;
+        String SzkolenieJava = "Szkolenie " + liczba;
+
+
+        System.out.println(SzkolenieJava);
+
+
+
+    }
+
+}
