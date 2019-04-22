9776cc89a6d2d0919b3020af03989de5030f82a0
ju <justyna.utko@ttpsc.pl>
2019-04-22 13:34:14 +0200

    zajecia2

---
+++ src/cw2.java
@@ -0,0 +1,13 @@
+public class cw2 {
+
+
+    public static void main(String[] args) {
+        zadanie1();
+    }
+
+        public static void zadanie1 () {
+            System.out.println("hello");
+        }
+
+    }
+
